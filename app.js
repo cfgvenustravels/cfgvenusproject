5feda3ec926f0c7725b072ffa45b055a6069e6e8
laurenokill <72352691+laurenokill@users.noreply.github.com>
2020-11-23 11:33:11 +0000

    jss for about

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-// creating a new click event
+<button onclick="document.location='default.asp'">Explore the World</button>
 
-var blogTitle = document.getElementsByClassName("blog-title")[0];
-
-// change the color of the text when mouse enters it
-
-// blogTitle.addEventListener('mouseenter', function(event){
-//     event.target.style.color = 'red'
-// })
-
-blogTitle[0].addEventListener('click' , function(event){
-    event.target.style.color = 'red'
-})
